c64c6bfa2370093d556f65b19d99076f0df2373a
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-11-27 19:48:27 -0300

    exec functions

--- Number.java
+++ Number.java
@@ -10,15 +10,26 @@ public class Number {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if (a > b && a > c) {
-            System.out.println("The largest number is: " + a);
-        } else if (b > a && b > c) {
-            System.out.println("The largest number is: " + b);
-        } else {
-            System.out.println("The largest number is: " + c);
-        }
+        int higher = max (a, b , c);
 
+        showResult(higher);
 
         sc.close();
     }
+public static int max(int x, int y, int z){
+        int aux;
+        if (x > y && x > z){
+            aux = x;
+        } else if (y > z){
+            aux = y;
+        } else {
+            aux = z;
+        }
+    return aux;
+}
+
+public static void showResult(int value){
+        System.out.println("Higher = " + value);
+    }
+
 }
